a3213d3088c0f134430ccf0a23ee8aefc7f2b5c7
Elena Popova <elipopovadev@gmail.com>
2021-12-16 23:25:53 +0200

    Tracker Library

--- TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
+++ TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
@@ -3,7 +3,7 @@
     public class MatchupEntryModel
     {
         public TeamModel TeamCompeting { get; set; }
-        public double Score{ get; set; }
+        public double Score { get; set; }
         public MatchupModel ParentMatchup { get; set; }
     }
 }
---
+++ TournamentTracker/TrackerLibrary/TeamModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+    }
+}
