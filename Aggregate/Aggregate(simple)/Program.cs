f8c139a5bba6b5de23ec7e77bc1a5db77a22d65d
Elena Popova <elipopovadev@gmail.com>
2021-12-22 01:54:13 +0200

    Aggregate(simple)

--- Aggregate/Aggregate(simple)/Program.cs
+++ Aggregate/Aggregate(simple)/Program.cs
@@ -1,8 +1,8 @@
-﻿int[] numbers = new int[] {1, 2, 3, 4};
-int result = numbers.Aggregate((a,b) => a + b);
+﻿int[] numbers = new int[] { 1, 2, 3, 4 };
+int result = numbers.Aggregate((a, b) => a + b);
 Console.WriteLine(result); // 10
 
-int[] numbers2 = new int[] {1,2,3,4};
+int[] numbers2 = new int[] { 1, 2, 3, 4 };
 int result2 = numbers2.Aggregate((a, b) => a * b);
 Console.WriteLine(result2); //24
 
