923ab2fb2c23c05463d73b6925d4a68643915c7e
Elena Popova <elipopovadev@gmail.com>
2021-12-22 02:04:44 +0200

    Aggregate(seed)

---
+++ Aggregate/Aggregate(seed)/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = new int[] { 1, 2, 3, 4 };
+int result = numbers.Aggregate(10, (a, b) => a + b);
+Console.WriteLine(result); // 20
+
+int[] numbers2 = new int[] { 1, 2, 3 };
+int result2 = numbers2.Aggregate(10, (a, b) => a * b);
+Console.WriteLine(result2); // 60
+
