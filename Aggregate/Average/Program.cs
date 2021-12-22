28eb998dae4ee3bfa32b03729e0ce8d55a06de0f
Elena Popova <elipopovadev@gmail.com>
2021-12-22 02:14:10 +0200

    Average

---
+++ Aggregate/Average/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = { 1, 2, 3, 4 };
+double result = numbers.Average();
+Console.WriteLine(result); // 2.5
+
+int[] numbers2 = { 20, 30, 10 };
+double result2 = numbers2.Average();
+Console.WriteLine(result2); // 20
+
+
