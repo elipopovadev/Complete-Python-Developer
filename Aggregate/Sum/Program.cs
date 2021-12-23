3ac844f0a08d8a1831f0d2c9afc8c0b038b1ce14
Elena Popova <elipopovadev@gmail.com>
2021-12-23 22:21:45 +0200

    Sum

---
+++ Aggregate/Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = new int[] { 1, 2, 3 };
+int sum = numbers.Sum();
+Console.WriteLine(sum); // 6
+
+int[] numbers2 = new int[] { 10, 20, 30, 40 };
+int sum2 = numbers2.Sum();
+Console.WriteLine(sum2); // 100
+
+
