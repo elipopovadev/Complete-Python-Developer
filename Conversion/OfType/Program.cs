8b43842afcf5e6b4a87b46ae1a1edc316d4844f2
Elena Popova <elipopovadev@gmail.com>
2021-12-25 00:04:19 +0200

    OfType

--- Conversion/OfType/Program.cs
+++ Conversion/OfType/Program.cs
@@ -4,3 +4,10 @@ foreach (var item in result)
 {
     Console.WriteLine(item); // Thomas, Joey
 }
+
+object[] objects2 = { "Thomas", 31, 5.02, null, "Joey" };
+var result2 = objects2.OfType<int>();
+foreach (var item in result2)
+{
+    Console.WriteLine(item); // 31
+}
