43a0dfed6bee36e2c84362241e6bde799b41b139
Elena Popova <elipopovadev@gmail.com>
2021-12-25 00:17:12 +0200

    OfType

--- Conversion/OfType/Program.cs
+++ Conversion/OfType/Program.cs
@@ -1,12 +1,12 @@
 ﻿object[] objects = { "Thomas", 31, 5.02, null, "Joey" };
-var result = objects.OfType<string>();
+IEnumerable<string> result = objects.OfType<string>();
 foreach (var item in result)
 {
     Console.WriteLine(item); // Thomas, Joey
 }
 
 object[] objects2 = { "Thomas", 31, 5.02, null, "Joey" };
-var result2 = objects2.OfType<int>();
+IEnumerable<int> result2 = objects2.OfType<int>();
 foreach (var item in result2)
 {
     Console.WriteLine(item); // 31
