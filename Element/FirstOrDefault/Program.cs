e08599254bddedb587289582e488019d0ed72ece
Elena Popova <elipopovadev@gmail.com>
2021-12-30 19:21:20 +0200

    FirstOrDefault

---
+++ Element/FirstOrDefault/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] countries = { "Denmark", "Sweden", "Norway" };
+string[] empty = { };
+
+string result1 = countries.FirstOrDefault();
+Console.WriteLine(result1); // Denmark
+
+string result2 = empty.FirstOrDefault();
+Console.WriteLine(result2); // null
