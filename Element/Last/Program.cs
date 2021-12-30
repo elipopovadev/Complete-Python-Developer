7d59323ee54b66e30098d67f95959ab64d6f7ec0
Elena Popova <elipopovadev@gmail.com>
2021-12-30 19:27:47 +0200

    Last

---
+++ Element/Last/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 10, 40 };
+int lastNumber = numbers.Last();
+Console.WriteLine(lastNumber); // 40
+
+string[] mountains = new string[] { "Mila", "Rila", "Rodopi", "Pirin" };
+string lastMountain = mountains.Last();
+Console.WriteLine(lastMountain); // Pirin
+
+string resultWithCurrentLength = mountains.Last(x => x.Length == 4);
+Console.WriteLine(resultWithCurrentLength); // Rila
