1093a7edc571dd22ed322b62a7e4eb874e535cfb
Elena Popova <elipopovadev@gmail.com>
2021-12-30 19:16:37 +0200

    First

---
+++ Element/First/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] towns = new string[] { "Varna", "Plovdiv", "Sofia" };
+string firstTowns = towns.First();
+Console.WriteLine(firstTowns); // Varna
+
+string[] mountains = new string[] { "Rila", "Pirin", "Rodopi" };
+string firstMountain = mountains.First();
+Console.WriteLine(firstMountain); // Rila
+
+string resultWithCurrentLength = towns.First(x => x.Length == 5);
+Console.WriteLine(resultWithCurrentLength); // Varna
