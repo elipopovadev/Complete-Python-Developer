a1aae6ec83470a33a5250ed3da6f4461421992bb
Elena Popova <elipopovadev@gmail.com>
2022-01-04 01:27:47 +0200

    DefaultIfEmpty

---
+++ Generation/DefaultIfEmpty/Program.cs
@@ -0,0 +1,25 @@
+﻿List<Pet> pets = new List<Pet> { { new Pet("Eli", 10) }, { new Pet("Gosho", 5) } };
+
+foreach (Pet pet in pets.DefaultIfEmpty())
+{
+    if (pet != null)
+    {
+        Console.WriteLine(pet.Name); // Eli, Gosho
+    }
+    else
+    {
+        throw new Exception("Pet is null");
+    }
+}
+
+
+public class Pet
+{
+    public Pet(string Name, int Age)
+    {
+        this.Name = Name;
+        this.Age = Age;
+    }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
