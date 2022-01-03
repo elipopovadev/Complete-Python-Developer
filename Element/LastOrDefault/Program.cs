44f88c4d1145089420bd915c3b671dec0d73ffe7
Elena Popova <elipopovadev@gmail.com>
2022-01-03 23:36:07 +0200

    LastOrDefault

---
+++ Element/LastOrDefault/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = new int[] {10, 9, 30,40,50,60};
+int lastNumber = numbers.LastOrDefault();
+Console.WriteLine(lastNumber); // 60
+
+int[] numbers2 = new int[4];
+int lastNumber2 = numbers2.LastOrDefault();
+
+int result = numbers.LastOrDefault( x => x <= 10); 
+Console.WriteLine(result); // 9
