ccbf493300c54c02cf0710b340f53cbd5a618fb6
Elena Popova <elipopovadev@gmail.com>
2022-01-03 23:48:06 +0200

    SingleOrDefault

---
+++ Element/SingleOrDefault/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = new int[] { 1 };
+int result = numbers.SingleOrDefault();
+Console.WriteLine(result); // 1
+
+int[] numbers2 = new int[] { 1, 2, 3 };
+try
+{
+    int result2 = numbers2.SingleOrDefault();
+    Console.WriteLine(result2);
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e); // System.InvalidOperationException: Sequence contains more than one element
+}
+
+int[] empty = { };
+Console.WriteLine(empty.SingleOrDefault()); // 0
+
+string[] emptyString = { };
+Console.WriteLine(emptyString.SingleOrDefault()); // null
+
+
