c1779fede405dee75078f7a896e1fd02bed44b53
Elena Popova <elipopovadev@gmail.com>
2022-01-07 23:12:42 +0200

    DefaultIfEmpty

--- Generation/DefaultIfEmpty/Program.cs
+++ Generation/DefaultIfEmpty/Program.cs
@@ -1,4 +1,21 @@
-﻿List<Pet> pets = new List<Pet> { { new Pet("Eli", 10) }, { new Pet("Gosho", 5) } };
+﻿string[] emptyStr = { };
+int[] emptyInt = { };
+string[] words = { "one", "two", "three" };
+
+var resultFirst = emptyStr.DefaultIfEmpty();
+var resulltSecond = emptyInt.DefaultIfEmpty();
+var resultThird = words.DefaultIfEmpty();
+
+Console.WriteLine(string.Join(", ", resultFirst)); // 0 for int
+Console.WriteLine(string.Join(", ", resulltSecond)); // null for string
+Console.WriteLine(string.Join(", ", resultThird)); // "one", "two", "three"
+
+
+/// <summary>
+/// Example with class
+/// </summary>
+
+List<Pet> pets = new List<Pet> { { new Pet("Eli", 10) }, { new Pet("Gosho", 5) } };
 
 foreach (Pet pet in pets.DefaultIfEmpty())
 {
