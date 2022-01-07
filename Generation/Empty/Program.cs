3bae7a906438db15afb201e8b10a8c94a8817d88
Elena Popova <elipopovadev@gmail.com>
2022-01-07 23:23:06 +0200

    Empty

---
+++ Generation/Empty/Program.cs
@@ -0,0 +1,9 @@
+﻿var empty = Enumerable.Empty<Student>();
+Console.WriteLine(empty.Count()); // 0
+Console.WriteLine(empty.GetType().Name); //EmptyPartition`1
+
+public class Student
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
