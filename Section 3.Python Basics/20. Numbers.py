d7d2d4f8dda5ee1f4b8497252ca74eb1aaeba2a8
Elena Popova <elipopovadev@gmail.com>
2022-01-13 13:19:19 +0200

    20. Numbers

---
+++ Section 3.Python Basics/20. Numbers.py
@@ -0,0 +1,29 @@
+#Division (float): returns float number
+print(6/2) 
+
+#Division (floor): returns int number
+print(6//2)
+
+#Modulus: returns the remainder when the first operand is divided by the second
+print(7%2)
+
+#Multiplication: multiplies two operands
+print(6*4)
+
+#Power: returns first raised to power second
+print(2**3)
+
+#Addition: adds two operands
+print(4+3)
+
+#Subtraction: subtracts two operands
+print(10-5)
+
+#Returns <class 'float'>
+print(type(4.043954323))
+
+#Returns <class 'int'>
+print(type(0))
+
+
+
