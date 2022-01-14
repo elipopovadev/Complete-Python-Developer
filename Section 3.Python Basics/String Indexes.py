78718b39ead0588b03989bfe5ab34fb868e9e2ea
Elena Popova <elipopovadev@gmail.com>
2022-01-14 17:26:38 +0200

    String Indexes

--- Section 3.Python Basics/String Indexes.py
+++ Section 3.Python Basics/String Indexes.py
@@ -1,20 +1,18 @@
 text = '01234567'
-print(text[0]) # print 0
-print(text[7]) # print 7
+print(text[0])  # print 0
+print(text[7])  # print 7
 
 #print(text[8]) IndexError: string index out of range
 
-print(text[-1]) # print 7
-print(text[-2]) # print 6
+print(text[-1])  # print 7
+print(text[-2])  # print 6
 
 # text[start:stop:stepover]
-print(text[0:2]) # print 01
-print(text[2:]) # starts from index 2; print 234567
-print(text[:8]) # 01234567
-print(text[0:8:2]) # 0246
-print(text[0:8:3]) # 036
+print(text[0:2])  # print 01
+print(text[2:])  # starts from index 2; print 234567
+print(text[:8])  # 01234567
+print(text[0:8:2])  # 0246
+print(text[0:8:3])  # 036
 
 # Reverse string
-print(text[: :-1]) #76543210
-
-
+print(text[::-1])  # 76543210
