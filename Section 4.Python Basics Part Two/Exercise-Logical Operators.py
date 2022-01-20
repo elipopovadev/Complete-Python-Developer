d4a4a5271093eb177ce21db926af4d32233f53b8
Elena Popova <elipopovadev@gmail.com>
2022-01-20 20:42:31 +0200

    Exercise-Logical Operators

---
+++ Section 4.Python Basics Part Two/Exercise-Logical Operators.py
@@ -0,0 +1,9 @@
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician and not is_expert:
+    print('at least you\'re getting there')
+elif not is_magician:
+    print('you need magic powers')
