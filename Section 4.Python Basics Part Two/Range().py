e95deeb85945cae6cb81485d8a7dd43aa2478fa8
Elena Popova <elipopovadev@gmail.com>
2022-01-20 23:09:30 +0200

    Range()

--- Section 4.Python Basics Part Two/Range().py
+++ Section 4.Python Basics Part Two/Range().py
@@ -1,8 +1,8 @@
-for num in range(0, 11):
-    print(num)
-    
-for num in range(11, -1, -1):
-    print(num)
+for num in range(0, 11, 2):
+    print(num)  
 
 for num in range(100, -1, -2):
     print(num)
+    
+for i in range(0, 11, 2):
+    print(list(range(0,11)))
