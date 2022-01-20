4035b1f1e21113c6bf5bb8e688dcbc062c4fc589
Elena Popova <elipopovadev@gmail.com>
2022-01-20 22:39:16 +0200

    Exercise-Tricky Counter

---
+++ Section 4.Python Basics Part Two/Exercise-Tricky Counter.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+sum = 0
+counter = 0
+
+for element in my_list:
+    sum += element
+    counter += 1
+
+print (f'The sum of elements is: {sum} and the counter is: {counter}')
+
+
+
