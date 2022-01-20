95f5b87868c8f7b30d47689eb5a44def595c7c18
Elena Popova <elipopovadev@gmail.com>
2022-01-20 19:41:26 +0200

    Ternary Operator

---
+++ Section 4.Python Basics Part Two/Ternary Operator.py
@@ -0,0 +1,9 @@
+# condition_if_true if condition else condition_if_false
+
+is_friend = True
+can_message = 'Message allowed.' if is_friend else 'Not allowed to message.'
+print(can_message)
+
+is_driver = True
+can_drive = 'The person has a licence.' if is_driver else 'The person does not have a licence.'
+print(can_drive)
