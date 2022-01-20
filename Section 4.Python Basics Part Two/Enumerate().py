55805d7197017b10e6fa798bbabef2bc00bad4d3
Elena Popova <elipopovadev@gmail.com>
2022-01-20 23:26:52 +0200

    Enumerate()

--- Section 4.Python Basics Part Two/Enumerate().py
+++ Section 4.Python Basics Part Two/Enumerate().py
@@ -7,10 +7,8 @@ for i, item in enumerate([1, 2, 3, 4, 5]):
 for i, item in enumerate({1, 2, 3, 4, 5}):
     print(f'{i} : {item}')
 
-
 for i, item in enumerate((10, 20, 39, 40, 54)):
     print(f'{i} : {item}')
-
 
 for i, item in enumerate({'name': 'Elena', 'age': 100}):
     print(f'{i} : {item}')  # 0 : name 1 : age
