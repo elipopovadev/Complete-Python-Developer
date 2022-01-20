6f031fbd7678b8384ca971bc2838c28f3dbb3713
Elena Popova <elipopovadev@gmail.com>
2022-01-20 18:35:20 +0200

    Conditional Logic

---
+++ Section 4.Python Basics Part Two/Conditional Logic.py
@@ -0,0 +1,9 @@
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+    print('You are old enough to drive, and you have a licence!')
+else:
+    print('You are not of age!')
+
+print('Final')
