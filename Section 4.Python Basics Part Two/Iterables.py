3c2aeaced652b65c05307b2db024bbb099bafb98
Elena Popova <elipopovadev@gmail.com>
2022-01-20 21:49:46 +0200

    Iterables

---
+++ Section 4.Python Basics Part Two/Iterables.py
@@ -0,0 +1,14 @@
+users = {
+    'name': 'Elena',
+    'age': 100,
+    'town': 'Varna'
+}
+
+for key, value in users.items():
+    print(key, value)
+
+for key in users.keys():
+    print(key)
+
+for value in users.values():
+    print(value)
