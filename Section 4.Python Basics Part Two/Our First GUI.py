5f8f78e56cce06345f4c963349a63375760fbb03
Elena Popova <elipopovadev@gmail.com>
2022-01-21 11:16:03 +0200

    Our First GUI

---
+++ Section 4.Python Basics Part Two/Our First GUI.py
@@ -0,0 +1,27 @@
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+for row in range(len(picture)):
+    for col in range(len(picture[row])):
+        if(picture[row][col] == 1):
+         print('*', end = '')
+        else:
+         print(' ', end = '')
+    print()
+    
+# Another way
+print()
+
+for line in picture:
+    for pixel in line:
+        if(pixel == 1):
+         print('*', end = '')
+        else:
+         print(' ', end = '')
+    print()
