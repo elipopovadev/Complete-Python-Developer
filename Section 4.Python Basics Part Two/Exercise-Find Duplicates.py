8396c8c3a5dd76a778a26a727f5b79a6c3845e14
Elena Popova <elipopovadev@gmail.com>
2022-01-21 12:58:12 +0200

    Exercise-Find Duplicates

--- Section 4.Python Basics Part Two/Exercise-Find Duplicates.py
+++ Section 4.Python Basics Part Two/Exercise-Find Duplicates.py
@@ -9,7 +9,7 @@ for element in some_list:
 
 for key, value in some_dictionary.items():
     if value > 1:
-       print(key, end = ' ')
+       print(key, end=' ')
 
 # another way
 duplicates = []
@@ -17,6 +17,7 @@ duplicates = []
 for x in some_list:
     if some_list.count(x) > 1 and x not in duplicates:
        duplicates.append(x)
-        
+
 print()
 print(duplicates)
+
