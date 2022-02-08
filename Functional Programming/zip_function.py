8f825d180dca81bddd1379f83a9ef12889e8ff18
Elena Popova <elipopovadev@gmail.com>
2022-02-08 14:01:01 +0200

    zip function

---
+++ Functional Programming/zip_function.py
@@ -0,0 +1,5 @@
+names = ["Elena", "Mariq", "Geri"]
+phone_numbers = ["+359 123", "+359 235", "+359 536"]
+
+new_list = (list(zip(names, phone_numbers)))
+print(new_list) # [('Elena', '+359 123'), ('Mariq', '+359 235'), ('Geri', '+359 536')]
