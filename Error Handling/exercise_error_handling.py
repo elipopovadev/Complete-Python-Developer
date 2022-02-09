be01e32fd15ca196f5cc566e2d9b16c96deb238a
Elena Popova <elipopovadev@gmail.com>
2022-02-09 18:29:31 +0200

    exercise error handling

---
+++ Error Handling/exercise_error_handling.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        age = int(input("What is your age?"))
+        10 / age
+    except ValueError as error:
+        raise error
+    except ZeroDivisionError as error:
+        raise error
+    else: 
+        print("Thank you")
+    finally:
+        print("I am finally done!") 
+        
+    
+    
