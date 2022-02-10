679d4497ae36ec2d44f9301004bf079e754809bc
Elena Popova <elipopovadev@gmail.com>
2022-02-11 00:42:51 +0200

    password validation

--- Regular Expressions/password_validation.py
+++ Regular Expressions/password_validation.py
@@ -1,12 +1,13 @@
 import re
 # At least 8 char long 
 # Contain any sort letters, numbers and $%#@
+# Has to end with number
 
-pattern = re.compile(r"(^[\w$%#@]{7}[\w$%#@]+$)")
-passwords = ["edgde4gtg%@", "ffefefe%%%", "$5swdwd#fe", "---fefe---", "12er"]
+pattern = re.compile(r"(^[\w$%#@]{7,}[\d]+$)")
+passwords = ["edgde4gtg%@3", "ffefefe%%%4", "$5swdwd#fe63", "---fefe---", "12er5"]
 
 print("Valid passwords are:")
 for password in passwords:
     if pattern.match(password):
-        print(password) # edgde4gtg%@, ffefefe%%%, $5swdwd#fe
+        print(password) # edgde4gtg%@3, ffefefe%%%4, $5swdwd#fe63
         
