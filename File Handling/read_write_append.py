b61f06ed9ff76be5bddd3c6ed5593f667dba753d
Elena Popova <elipopovadev@gmail.com>
2022-02-10 17:59:07 +0200

    read write append

--- File Handling/read_write_append.py
+++ File Handling/read_write_append.py
@@ -1,2 +1,16 @@
-with open('test.txt', mode='r') as my_file:
-    print(my_file.read())
+my_file = open('test.txt', mode = 'w')
+my_file.write('Hello!')
+my_file.close()
+
+my_file = open('test.txt', mode = 'a')
+my_file.write(" My name is Elena!")
+my_file.write(" How are you?")
+my_file.close()
+
+try:
+  my_file = open('test.txt', mode = 'r')
+  for line in my_file:
+     print(line)
+  my_file.close()
+except FileNotFoundError as error:
+    raise error
