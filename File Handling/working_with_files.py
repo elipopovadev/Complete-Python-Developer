71f2a3e0a87d302d224f07e9f354ec585b0f6bee
Elena Popova <elipopovadev@gmail.com>
2022-02-10 14:56:20 +0200

    working with files

---
+++ File Handling/working_with_files.py
@@ -0,0 +1,8 @@
+my_file = open('test.txt')
+print(my_file.readline())
+print(my_file.readline())
+print(my_file.readline())
+
+# print(my_file.read)
+# print(my_file.readlines()) # ['Hi, my name is Elena Popova!\n', ':)\n', 'How are you?']
+my_file.close()
