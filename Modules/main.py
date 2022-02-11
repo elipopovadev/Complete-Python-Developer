0e9990fade6287b04853989669792cc5317a9d49
Elena Popova <elipopovadev@gmail.com>
2022-02-11 21:44:40 +0200

    modules

--- Modules/main.py
+++ Modules/main.py
@@ -3,3 +3,4 @@ import utility
 print(utility.multiply(4, 4))
 print(utility.sum(4, 3))
 print(utility.deviation(10, 2))
+
--- Modules/utility.py
+++ Modules/utility.py
@@ -1,9 +1,8 @@
 def multiply(num1, num2):
     return num1 * num2
 
-def sum(num1, num2):
-    return num1 + num2
-
 def deviation(num1, num2):
     return num1 / num2
-        
+
+def sum(num1, num2):
+    return num1 + num2
