53c03ea667ebb9c8e8be4aa310f9b793887fc0fe
Elena Popova <elipopovadev@gmail.com>
2022-02-11 21:42:59 +0200

    exercise utility

---
+++ Modules/main.py
@@ -0,0 +1,5 @@
+import utility
+
+print(utility.multiply(4, 4))
+print(utility.sum(4, 3))
+print(utility.deviation(10, 2))
---
+++ Modules/utility.py
@@ -0,0 +1,9 @@
+def multiply(num1, num2):
+    return num1 * num2
+
+def sum(num1, num2):
+    return num1 + num2
+
+def deviation(num1, num2):
+    return num1 / num2
+        
