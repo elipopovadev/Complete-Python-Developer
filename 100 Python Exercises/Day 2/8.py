0c83c5fba93575b0bfffd8bb5a8cc33df46beaa6
Elena Popova <elipopovadev@gmail.com>
2022-03-06 00:20:16 +0200

    Day 2

---
+++ 100 Python Exercises/Day 2/8.py
@@ -0,0 +1,11 @@
+# Write a program that accepts a comma separated sequence of words as input and prints
+# the words in a comma-separated sequence after sorting them alphabetically.
+# Suppose the following input is supplied to the program:
+# without,hello,bag,world
+# Then, the output should be:
+# bag,hello,without,world
+
+input_array = input().split(',')
+sorted_array = sorted(input_array, key = str.lower)
+print(','.join(sorted_array))
+
